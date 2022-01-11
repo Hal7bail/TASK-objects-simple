c94b8a3f915e0ad30eae157d82655da298d6ace8
hal7bail <hal7bail@gmail.com>
2022-01-11 20:21:22 +0300

    done

--- index.js
+++ index.js
@@ -13,6 +13,32 @@
 
 // add the movies variables here ....
 
+const movie1 = {
+  id: "1",
+  name: "The Hobbit",
+  image:
+    "https://www.pluggedin.com/wp-content/uploads/2020/01/hobbit-cover-1340x2048.jpg",
+  rating: 7.8,
+};
+const movie2 = {
+  id: "2",
+  name: "The gladiator",
+  image:
+    "https://d2e111jq13me73.cloudfront.net/sites/default/files/styles/product_image_aspect_switcher_170w/public/product-images/csm-movie/gladiator-poster.jpg?itok=wJ_O8kje",
+  rating: 8.5,
+};
+const movie3 = {
+  id: "3",
+  name: "COCO",
+  image:
+    "https://m.media-amazon.com/images/M/MV5BYjQ5NjM0Y2YtNjZkNC00ZDhkLWJjMWItN2QyNzFkMDE3ZjAxXkEyXkFqcGdeQXVyODIxMzk5NjA@._V1_.jpg",
+  rating: 8.4,
+};
+
+console.log(movie1);
+console.log(movie2);
+console.log(movie3);
+
 /** Task 2:
  * Create an array `movies`
  * Add to it the movies that you created on top.
@@ -29,6 +55,27 @@
  *    },
  * ]
  */
+
+movies = [
+  {
+    name: "The Hobbit",
+    image:
+      "https://www.pluggedin.com/wp-content/uploads/2020/01/hobbit-cover-1340x2048.jpg",
+    rating: 7.8,
+  },
+  {
+    name: "The gladiator",
+    image:
+      "https://d2e111jq13me73.cloudfront.net/sites/default/files/styles/product_image_aspect_switcher_170w/public/product-images/csm-movie/gladiator-poster.jpg?itok=wJ_O8kje",
+    rating: 8.5,
+  },
+  {
+    name: "COCO",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BYjQ5NjM0Y2YtNjZkNC00ZDhkLWJjMWItN2QyNzFkMDE3ZjAxXkEyXkFqcGdeQXVyODIxMzk5NjA@._V1_.jpg",
+    rating: 8.4,
+  },
+];
 
 /** TASK 3:
  * Create a new file call it `movies.json`
